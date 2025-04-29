ece940018ced17fe08f0ca62459acf9db7de8a1b
Carlos Eduardo <ducameneses16@gmail.com>
2025-04-29 03:10:56 -0300

    feat: update settings and urls configuration

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,12 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
 from home.views import home_view
 from users.views import profile_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('my-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', home_view, name='home'),
     path('profile/', include('users.urls')),
@@ -33,3 +32,4 @@ urlpatterns = [
 
 # Use in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
